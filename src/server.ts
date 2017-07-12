9fd4e90ab5f8efe1b2aebb5b00280d194b58f73f
vijay vittal <vijay.vittal@votercircle.com>
2017-07-12 14:50:42 +0530

    url updated

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ export class Server {
 
         // CONFIG mongo database
         (<any>mongoose).Promise = global.Promise;
-        mongoose.connect('mongodb://localhost:1337/vittal');
+        mongoose.connect('mongodb://192.168.1.200:1337/vittal');
         console.log("successfully connected to MONGODB");
     }
 
